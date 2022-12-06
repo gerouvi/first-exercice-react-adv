a960c86ac208858ddf6a5566994fbf23b2814f4b
Gerard <g.roura.v@protonmail.com>
2022-12-06 14:49:31 +0000

    refactor sizeString

--- src/components/atoms/Circle.jsx
+++ src/components/atoms/Circle.jsx
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types'
 
 export function Circle({ size, color }) {
-  const sizeString = String(size)
   return (
     <div>
       <svg width={size} height={size} fill={color}>
-        <circle cx={sizeString / 2} cy={sizeString / 2} r={sizeString / 2} />
+        <circle cx={size / 2} cy={size / 2} r={size / 2} />
       </svg>
     </div>
   )
